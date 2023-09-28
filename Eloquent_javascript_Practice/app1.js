4fae9f70726206e94e7121ebc59a28b9df8a923b
Sudeepa Pal <85659225+sudeepa88@users.noreply.github.com>
2023-09-29 00:27:42 +0530

    still learning

---
+++ Eloquent_javascript_Practice/app1.js
@@ -0,0 +1,16 @@
+//binding function
+
+// const okay= function(x){
+//     return x*x;
+// };
+
+// console.log(okay(12));
+
+let x=10;
+if(true){
+    let y=20;
+    var z=30;
+    console.log(x+y+z);
+}
+
+console.log(x+z);
